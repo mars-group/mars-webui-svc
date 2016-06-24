e209e3c5b81a8010a6e5d2431cf70832b063690c
Lennart K <lennart.karsten@haw-hamburg.de>
2016-06-24 18:12:55 +0200

    fix production and make home page active

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ var connect = require('connect');
 var serveStatic = require('serve-static');
 var eureka = require('./components/eureka');
 
-connect().use(serveStatic(__dirname + '/build')).listen(3000, function () {
+connect().use(serveStatic('./dist')).listen(3000, function () {
   console.log('Server running on port 3000 in production mode ...');
 
   eureka.register();
