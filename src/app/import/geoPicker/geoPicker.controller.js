986536821dd00b7ae76bdc18158451085a3a3903
Lennart K <lennart.karsten@haw-hamburg.de>
2016-07-21 19:10:20 +0200

    add missing files

---
+++ src/app/import/geoPicker/geoPicker.controller.js
@@ -0,0 +1,47 @@
+(function () {
+  'use strict';
+
+  angular.module('test')
+    .controller('GeoPickerController', GeoPickerController);
+
+  /** @ngInject */
+  function GeoPickerController($scope, $uibModalInstance, marker) {
+    var vm = this;
+
+    // the marker has to be an array for leaflet
+    if (marker) {
+      $scope.marker = [marker];
+    }
+
+    // map events (don't remove it)
+    $scope.events = {};
+
+    // center map
+    vm.center = {
+      lat: 0,
+      lng: 0,
+      zoom: 2
+    };
+
+    // on click
+    $scope.$on("leafletDirectiveMap.click", function(event, args){
+      var leafEvent = args.leafletEvent;
+
+      // keep just one marker
+      $scope.marker = [{
+        lat: leafEvent.latlng.lat,
+        lng: leafEvent.latlng.lng
+      }];
+    });
+
+    vm.ok = function () {
+      $uibModalInstance.close($scope.marker[0]);
+    };
+
+    vm.cancel = function () {
+      $uibModalInstance.dismiss('cancel');
+    };
+
+  }
+
+})();
