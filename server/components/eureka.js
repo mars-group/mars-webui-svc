7785be81802fe84f96fcb9b5e76ecef081e13ce1
Lennart K <lennart.k@thinking-aloud.eu>
2016-08-15 08:35:53 +0200

    change-eureka-url

--- server/components/eureka.js
+++ server/components/eureka.js
@@ -1,49 +1,65 @@
 'use strict';
 
+const Eureka = require('eureka-js-client').Eureka;
 var os = require('os');
-var request = require('superagent');
+
+var ifaces = os.networkInterfaces();
+var ip = '127.0.0.1';
+
+
+Object.keys(ifaces).forEach(function (ifname) {
+  var alias = 0;
+
+  ifaces[ifname].forEach(function (iface) {
+    if ('IPv4' !== iface.family || iface.internal !== false) {
+      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+      return;
+    }
+
+    if (iface.address) {
+      ip = iface.address;
+    }
+
+    ++alias;
+  });
+});
+
+const client = new Eureka({
+  instance: {
+    app: 'frontend',
+    hostName: os.hostname(),
+    ipAddr: ip,
+    statusPageUrl: 'http://' + os.hostname() + ':3000',
+    port: {
+      '$': 3000,
+      '@enabled': 'true'
+    },
+    vipAddress: 'frontend',
+    dataCenterInfo: {
+      '@class': 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
+      name: 'MyOwn'
+    }
+  },
+  eureka: {
+    // eureka server host / port
+    host: 'eureka',
+    port: 8080,
+    servicePath: '/eureka/apps/',
+    fetchRegistry: true
+  }
+});
 
 module.exports = {
   register: function () {
-    //Register at EUREKA
-    request.post('http://eureka:8080/eureka/v2/apps/frontend')
-      .set('Content-Type', 'application/xml')
-      .send(
-        '<?xml version=\'1.0\' encoding=\'utf-8\'?>' +
-        '<instance>' +
-        '  <hostName>' + os.hostname() + '</hostName>' +
-        '  <app>frontend</app>' +
-        '  <ipAddr></ipAddr>' +
-        '  <vipAddress></vipAddress>' +
-        '  <secureVipAddress></secureVipAddress>' +
-        '  <status>UP</status>' +
-        '  <port>8080</port>' +
-        '  <securePort>8080</securePort>' +
-        '  <dataCenterInfo>' +
-        '    <name>MyOwn</name>' +
-        '  </dataCenterInfo>' +
-        '</instance>'
-      ).end(function (regErr, regRes) {
 
-      if (regErr) {
-        console.log('REGISTER ERROR: ', regErr);
-        return false;
-      }
-
-      console.log('Successfully registered at Eureka');
+    setTimeout(function () {
+      client.start();
 
       setInterval(function () {
-        sendHeartBeatToEureka();
-      }, 20000);
-    });
+        client.renew();
+      }, 30000);
+    },40000);
+
   }
 };
 
-//Send heartbeat to EUREKA
-var sendHeartBeatToEureka = function () {
-  request.put('http://eureka:8080/eureka/v2/apps/frontend/' + os.hostname()).end(function (regErr, regRes) {
-    if (regErr) {
-      console.log('REGISTER ERROR: ', regErr);
-    }
-  });
-};
