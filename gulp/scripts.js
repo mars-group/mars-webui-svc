328b201fe5c12f1de595940850b86372457bedae
Lennart K <lennart.karsten@haw-hamburg.de>
2016-08-17 14:36:29 +0200

    Remove crappy WEBGL JavaScript file from gulp checking

--- gulp/scripts.js
+++ gulp/scripts.js
@@ -9,18 +9,21 @@ var browserSync = require('browser-sync');
 var $ = require('gulp-load-plugins')();
 
 
-gulp.task('scripts-reload', function() {
+gulp.task('scripts-reload', function () {
   return buildScripts()
     .pipe(browserSync.stream());
 });
 
-gulp.task('scripts', function() {
+gulp.task('scripts', function () {
   return buildScripts();
 });
 
 function buildScripts() {
-  return gulp.src(path.join(conf.paths.src, '/app/**/*.js'))
+  return gulp.src([
+    path.join(conf.paths.src, '/app/**/*.js'),
+    path.join('!' + conf.paths.src, '/app/webgl/data/UnityLoader.js')
+  ])
     .pipe($.eslint())
     .pipe($.eslint.format())
     .pipe($.size())
-};
+}
