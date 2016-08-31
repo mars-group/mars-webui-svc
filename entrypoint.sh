d3155e149075c0fbfd5565e2386630bed6574b2c
Lennart K <lennart.karsten@haw-hamburg.de>
2016-08-31 15:58:05 +0200

    delete sources after building the production Frontend

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,35 @@
 #!/usr/bin/env bash
 
 if [ "$DEVELOPER_EDITION" = "true" ]; then
-        echo "starting development ..."
-        npm install
-        bower install --allow-root
-        gulp serve --color
-#        tail -f /dev/null
-    else
-        echo "starting production ..."
-        gulp
-        node server/app.js
-    fi
+
+  echo "starting development ..."
+
+  npm install
+  bower install --allow-root
+
+  gulp serve --color
+#  tail -f /dev/null
+
+else
+  echo "starting production ..."
+
+  # build dist
+  gulp
+
+  # move prod files
+  mkdir /prod
+  cd /prod
+  mv /app/server .
+  mv /app/dist .
+  mv /app/package.json .
+
+  # install production dependencies
+  npm install --only=production
+
+  # remove sources
+  rm -rf /app
+
+  # start server
+  node server/app.js
+
+fi
