9775d62482a90d5aff55d8447938bc071e9dfd8b
Lennart K <lennart.karsten@haw-hamburg.de>
2016-09-26 18:47:53 +0200

    continue mapping stuff

--- src/app/mapping/mapping.controller.js
+++ src/app/mapping/mapping.controller.js
@@ -11,7 +11,21 @@
 
     vm.data = null;
     vm.selectedNode = null;
-    vm.selectedField = null;
+    vm.selectedAgent = null;
+    vm.treeExpandedNodes = [];
+
+
+    Object.prototype.renameProperty = function (oldName, newName) {
+      if (oldName == newName) {
+        return this;
+      }
+
+      if (this.hasOwnProperty(oldName)) {
+        this[newName] = this[oldName];
+        delete this[oldName];
+      }
+      return this;
+    };
 
     var initMappingData = function () {
       vm.treeOptions = {
@@ -23,32 +37,54 @@
     };
     initMappingData();
 
-    Mapping.getMapping(function (mapping) {
-      var tmp = [];
-      angular.forEach(mapping, function layerTypes(value, key) {
-        tmp.push({
-          name: key,
+    var loadFieldsToMap = function () {
+      Mapping.getMapping(function (mapping) {
+        vm.data = mapping;
+        prepareMappingFieldStructureForTreeView(configureTreeView);
+      });
+    };
+    loadFieldsToMap();
+
+
+    var prepareMappingFieldStructureForTreeView = function (callback) {
+      var tmpTreeData = [];
+
+      angular.forEach(vm.data, function layerTypes(value, key) {
+        var layerType = {
+          Name: key,
           children: value
+        };
+
+        angular.forEach(layerType.children, function layers(value2, key2) {
+
+          layerType.children[key2].renameProperty('Agents', 'children');
         });
+
+        tmpTreeData.push(layerType);
       });
 
-      vm.treeData = tmp;
-      vm.treeExpandedNodes = [vm.treeData[0], vm.treeData[1], vm.treeData[2]];
+      vm.treeData = tmpTreeData;
+      callback();
+    };
+
+    var configureTreeView = function () {
+      angular.forEach(vm.treeData, function (value, key) {
+        vm.treeExpandedNodes.push(value);
+      });
 
       // for debugging only
-      vm.selectedNode = vm.treeData[0].children[1];
-      console.log('treeData:', vm.treeData);
-      console.log('selectedNode', vm.selectedNode);
-    });
+      vm.selectedNode = vm.treeData[0].children[1].children[0];
+      vm.treeExpandedNodes = [vm.treeData[0], vm.treeData[1], vm.treeData[2], vm.treeData[0].children[1]];
+    };
 
     vm.save = function () {
       console.log(vm.data);
       // TODO: write data back to original structure
     };
 
-    vm.selectField = function (field) {
-      vm.selectedField = field;
-      console.log(field);
+    vm.startMapping = function (agent) {
+      vm.selectedAgent = agent;
+      console.log('selectedAgent:', agent);
     };
 
   }
