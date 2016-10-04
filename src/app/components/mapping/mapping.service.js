1a3984395cb46b304a0415f7e5dea7a4c55f59d9
Lennart K <lennart.karsten@haw-hamburg.de>
2016-10-04 17:29:47 +0200

    Update mapping_mock and move data converting to a directive

--- src/app/components/mapping/mapping.service.js
+++ src/app/components/mapping/mapping.service.js
@@ -4,11 +4,47 @@
   angular
     .module('marsApp')
     .factory('Mapping', function Mapping($http) {
+      var prepareMappingFieldStructureForTreeView = function (data, callback) {
+        var tmpTreeData = [];
+
+        angular.forEach(data.InitializationDescription, function layerTypes(value, key) {
+          if (!angular.equals(key, 'LogicalChangeTime')) {
+
+            var layerType = {
+              Name: key,
+              children: value
+            };
+
+            angular.forEach(layerType.children, function layers(value2, key2) {
+              renameProperty(layerType.children[key2], 'Agents', 'children');
+            });
+
+            tmpTreeData.push(layerType);
+          }
+        });
+
+        callback(tmpTreeData);
+      };
+
+      var renameProperty = function (objRef, oldName, newName) {
+        if (oldName == newName) {
+          return objRef;
+        }
+
+        if (objRef.hasOwnProperty(oldName)) {
+          objRef[newName] = objRef[oldName];
+          delete objRef[oldName];
+        }
+        return objRef;
+      };
+
       return {
         getMapping: function (callback) {
           $http.get('app/components/mapping/mapping_mock.json')
             .then(function (res) {
-              callback(res.data);
+              prepareMappingFieldStructureForTreeView(res.data, function (treeData) {
+                return callback(treeData);
+              });
             });
         }
       };
--- src/app/mapping/mapping.controller.js
+++ src/app/mapping/mapping.controller.js
@@ -9,7 +9,6 @@
   function MappingController(Mapping, Metadata) {
     var vm = this;
 
-    vm.data = null;
     vm.metadata = null;
     vm.treeExpandedNodes = [];
     vm.selectedNode = null;
@@ -26,77 +25,49 @@
     };
     initMappingData();
 
-    var loadFieldsToMap = function () {
+    var loadMappingFields = function () {
       Mapping.getMapping(function (mapping) {
-        vm.data = mapping;
-        prepareMappingFieldStructureForTreeView(configureTreeView);
+        vm.treeData = mapping;
+        configureTreeView();
       });
     };
-    loadFieldsToMap();
-
-
-    var prepareMappingFieldStructureForTreeView = function (callback) {
-      var tmpTreeData = [];
-
-      angular.forEach(vm.data, function layerTypes(value, key) {
-        var layerType = {
-          Name: key,
-          children: value
-        };
-
-        angular.forEach(layerType.children, function layers(value2, key2) {
-
-          renameProperty(layerType.children[key2], 'Agents', 'children');
-        });
-
-        tmpTreeData.push(layerType);
-      });
-
-      vm.treeData = tmpTreeData;
-      callback();
-    };
-
-    var renameProperty = function (objRef, oldName, newName) {
-      if (oldName == newName) {
-        return objRef;
-      }
-
-      if (objRef.hasOwnProperty(oldName)) {
-        objRef[newName] = objRef[oldName];
-        delete objRef[oldName];
-      }
-      return objRef;
-    };
+    loadMappingFields();
 
     var configureTreeView = function () {
+      expandTopLevelNodes();
+
+      // for debugging only
+      vm.selectedNode = vm.treeData[0].children[0].children[0];
+      vm.treeExpandedNodes.push(vm.treeData[0].children[0]);
+    };
+
+    var expandTopLevelNodes = function () {
       angular.forEach(vm.treeData, function (value /*, key*/) {
         vm.treeExpandedNodes.push(value);
       });
-
-      // for debugging only
-      vm.selectedNode = vm.treeData[0].children[1].children[0];
-      vm.treeExpandedNodes = [vm.treeData[0], vm.treeData[1], vm.treeData[2], vm.treeData[0].children[1]];
     };
 
-    Metadata.getAll(function getAllFinishedImports(res) {
-      var metadata = [];
-      angular.forEach(res.data, function (element) {
-        if (angular.equals(element.state, 'FINISHED')) {
-          metadata.push(element);
-        }
+    var loadMappingDatasets = function() {
+      Metadata.getAll(function getAllFinishedImports(res) {
+        var metadata = [];
+        angular.forEach(res.data, function (element) {
+          if (angular.equals(element.state, 'FINISHED')) {
+            // writes to tmp to prevent constant refreshing in the view
+            metadata.push(element);
+          }
+        });
+        vm.metadata = metadata;
       });
-      vm.metadata = metadata;
-    });
+    };
+    loadMappingDatasets();
 
     vm.createMapping = function (dataset) {
-      vm.selectedField.TableName =  dataset.title;
+      vm.selectedField.TableName = dataset.title;
     };
 
     vm.save = function () {
-      // console.log(vm.data);
       // TODO: write data back to original structure
     };
 
   }
-
 })();
