838ec930e62dcb8840b4451b30868b44073d492d
Lennart K <lennart.karsten@haw-hamburg.de>
2016-10-19 13:31:50 +0200

    update to newer syntax

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,31 @@
+(function () {
+  'use strict';
 /**
  *  Welcome to your gulpfile!
  *  The gulp tasks are split into several files in the gulp directory
  *  because putting it all here was too long
  */
 
-'use strict';
+  var gulp = require('gulp');
+  var wrench = require('wrench');
 
-var gulp = require('gulp');
-var wrench = require('wrench');
-
-/**
- *  This will load all js or coffee files in the gulp directory
- *  in order to load all gulp tasks
- */
-wrench.readdirSyncRecursive('./gulp').filter(function(file) {
-  return (/\.(js|coffee)$/i).test(file);
-}).map(function(file) {
-  require('./gulp/' + file);
-});
+  /**
+   *  This will load all js or coffee files in the gulp directory
+   *  in order to load all gulp tasks
+   */
+  wrench.readdirSyncRecursive('./gulp').filter(function (file) {
+    return (/\.(js|coffee)$/i).test(file);
+  }).map(function (file) {
+    require('./gulp/' + file);
+  });
 
 
-/**
- *  Default task clean temporaries directories and launch the
- *  main optimization build task
- */
-gulp.task('default', ['clean'], function () {
-  gulp.start('build');
-});
+  /**
+   *  Default task clean temporaries directories and launch the
+   *  main optimization build task
+   */
+  gulp.task('default', ['clean'], function () {
+    gulp.start('build');
+  });
+
+})();
