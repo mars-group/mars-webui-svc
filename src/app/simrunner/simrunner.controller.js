fa1384eef52c1ecbae12688c4604c8a19ab80632
Lennart K <lennart.karsten@haw-hamburg.de>
2017-01-17 15:44:00 +0100

    remove gulp warning

--- src/app/simrunner/simrunner.controller.js
+++ src/app/simrunner/simrunner.controller.js
@@ -23,13 +23,13 @@
     vm.SimPlanName = "";
 
     vm.CreateSimPlan = function(){
-      SimRunner.createSimPlan(vm.SimPlanName, vm.ScenarioId, "42", "42", function(res) {
+      SimRunner.createSimPlan(vm.SimPlanName, vm.ScenarioId, "42", "42", function(/*res*/) {
 
       });
     };
 
     vm.StartSimulationRun = function(simPlanId){
-      SimRunner.startSimPlan(simPlanId, function(res){});
+      SimRunner.startSimPlan(simPlanId, function(/*res*/){});
     };
   }
 })();
