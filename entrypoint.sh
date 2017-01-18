31eaa2603bd907e70704fe232ff7cdefe04b6c82
Lennart K <lennart.karsten@haw-hamburg.de>
2017-01-18 20:50:02 +0100

    test new build pipeline

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,12 @@
 #!/usr/bin/env bash
 
-if [ "$DEVELOPER_EDITION" = "true" ]; then
-  echo "starting development ..."
+echo "starting development ..."
 
-  echo "starting npm install ..."
-  npm install
+echo "starting npm install ..."
+npm install
 
-  echo "starting npm install ..."
-  bower install
+echo "starting npm install ..."
+bower install
 
-  gulp serve --color
+gulp serve --color
 #  tail -f /dev/null
-
-else
-  echo "starting production ..."
-
-  cd /prod
-
-  # start server
-  node server/app.js
-
-fi
