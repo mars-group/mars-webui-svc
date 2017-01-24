c3f11e3f759fd4d6f9b832e5cbc4905cab2b3976
Lennart K <lennart.karsten@haw-hamburg.de>
2017-01-24 11:27:10 +0100

    fix typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-echo "starting development ..."
+echo "starting development..."
 
-echo "starting npm install ..."
+echo "starting npm install..."
 npm install
 
-echo "starting npm install ..."
+echo "starting bower install..."
 bower install
 
+echo "starting the application..."
 gulp serve --color
-#  tail -f /dev/null
+#tail -f /dev/null
